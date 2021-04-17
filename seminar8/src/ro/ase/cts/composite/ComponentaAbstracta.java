23bea72eb20696f9380920b37f6345c2373b343a
Adrian Soare <asoare_29g@yahoo.com>
2021-04-17 12:48:07 +0300

    composite partea2 seminar

---
+++ seminar8/src/ro/ase/cts/composite/ComponentaAbstracta.java
@@ -0,0 +1,10 @@
+package ro.ase.cts.composite;
+
+public interface ComponentaAbstracta {
+    public void printareElement();
+    public void adaugaNod(ComponentaAbstracta componentaAbstracta);
+    public void stergeNod(ComponentaAbstracta componentaAbstracta);
+    public ComponentaAbstracta getNod(int pozitia);
+
+
+}
